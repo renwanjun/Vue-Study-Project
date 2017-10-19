10f2d6439a4126b3dd9ae9c1b48a82b4c232d1ed
renwj <994435941@qq.com>
2017-10-19 17:33:02 +0800

    添加工程文件

---
+++ src/components/Main.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <header class="public-header">
+          <div class="public-content clearfloat">
+              <div class="header-logo">
+                  <a href=""></a>
+              </div>
+              <ul class="header-nav">
+                  <li class="item"><a href="">Our Story</a></li>
+                  <li class="item"><a href="">Menu</a></li>
+                  <li class="item"><a href="">Peservations</a></li>
+                  <li class="item"><a href="">News</a></li>
+                  <li class="item"><a href="">Rviews</a></li>
+              </ul>
+          </div>
+      </header>
+      <div class="public-content index-banner"></div>
+      <div class="public-content index-list"></div>
+      <div class="public-content index-panel">
+          <div class="index-panel-header">FEATURED DISHES</div>
+      </div>
+      <div class="public-content index-panel">
+          <div class="index-panel-header">THE GALLERY</div>
+      </div>
+      <footer class="public-footer">
+          <div class="public-content">
+
+          </div>
+      </footer>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 引入全局css
+import './common/styles/reset.css'
+import './common/styles/common.less'
+import './common/styles/virables.less'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
