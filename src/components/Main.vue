787adbf2155c74e5efb2f40aba786d2e4c3176b0
renwj <renwanjun@lanjingren.com>
2018-04-08 21:05:21 +0800

    update

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -18,25 +18,12 @@
           </div>
       </header>
 
-    <div class="over1">
-      你懂不懂我的心，其实我就是想试试两行要怎么解决你懂不懂我的心，其实我就是想试试两行要怎么解决你懂不懂我的心，其实我就是想试试两行要怎么解决你懂不懂我的心，其实我就是想试试两行要怎么解决你懂不懂我的心，其实我就是想试试两行要怎么解决你懂不懂我的心，其实我就是想试试两行要怎么解决你懂不懂我的心，其实我就是想试试两行要怎么解决你懂不懂我的心，其实我就是想试试两行要怎么解决
-    </div>
-
-    <div class="over2">
-      你懂不懂我的心，其实我就是想试试两行要怎么解决你懂不懂我的心，其实我就是想试试两行要怎么解决你懂不懂我的心，其实我就是想试试两行要怎么解决你懂不懂我的心，其实我就是想试试两行要怎么解决你懂不懂我的心，其实我就是想试试两行要怎么解决你懂不懂我的心，其实我就是想试试两行要怎么解决你懂不懂我的心，其实我就是想试试两行要怎么解决你懂不懂我的心，其实我就是想试试两行要怎么解决
-    </div>
-
-    <div style="width: 100px;height: 100px;">
-      <div class="zxx_text_overflow" >
-        <div class="zxx_con" >这是一段比较长的文字，用来测试是否文字溢出时会用省略号显示。</div>
-        <div class="zxx_dotted" >…</div>
-      </div>
-    </div>
-
-
 
     <!--<starry-sky></starry-sky>-->
     <!--<picture-carousel></picture-carousel>-->
+    <!--<test></test>-->
+      <!--<svgs>< /svgs>-->
+    <three-d></three-d>
 
       <div class="public-content index-banner"></div>
       <div class="public-content index-list"></div>
@@ -54,63 +41,22 @@
   </div>
 </template>
 <style lang="less" scoped>
-  .over1{
-    background: antiquewhite;
-    margin: 20px;
-    /*padding: 20px;*/
-    width: 100px;
-    /*border:solid 1px red;*/
-    /*white-space:nowrap;*/
-    overflow: hidden;
-    -o-text-overflow: ellipsis;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-line-clamp: 6;
-    -webkit-box-orient: vertical;
-    word-break: break-all;
-    max-height: 156px;
-  }
 
-  .over2{
-    background: antiquewhite;
-    margin: 20px;
-    /*padding: 20px;*/
-    width: 100px;
-    overflow: hidden;
-    word-break: break-all;
-    max-height: 156px;
-    &:after{
-      position: absolute;
-      bottom: 0;
-      right: 0;
-      padding:0 5px 1px 45px;
-      content: '...';
-      /*!*background:url(ellipsis_bg.png) repeat-y;*!*/
-      background:-webkit-linear-gradient(left,transparent,#fff55%);
-      background:-o-linear-gradient(right,transparent,#fff55%);
-      background:-moz-linear-gradient(right,transparent,#fff55%);
-      background:linear-gradient(toright,transparent,#fff55%);
-    }
-
-    .zxx_text_overflow{
-      width:100px;
-      height:1.3em;
-      overflow:hidden;
-      zoom:1;
-    }
-    .zxx_text_overflow .text_con{float:left; height:1.3em; margin-right:3em; overflow:hidden;}
-    .zxx_text_overflow .text_dotted{width:3em; height:1.31em; float:right; margin-top:-1.3em;}
-  }
 
 </style>
 <script>
 import StarrySky from './animation/starry_sky'
 import PictureCarousel from './animation/picture_carouse'
-
+import Test from './test'
+import Svgs from './svg'
+import ThreeD from './threeD'
 export default {
   components: {
     StarrySky,
-    PictureCarousel
+    PictureCarousel,
+    Test,
+    Svgs,
+    ThreeD
   },
   name: 'Main',
   data () {
---
+++ src/components/svg.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <svg version="1.1"
+           baseProfile="full"
+           width="300" height="200"
+           xmlns="http://www.w3.org/2000/svg">
+        <rect width="100%" height="100%" fill="pink" />
+        <circle cx="150" cy="100" r="90" fill="yellow" />
+        大是大非上
+      </svg>
+    </div>
+</template>
+<style lang="less" scoped>
+  svg{
+    circle{
+      cx:100;
+      fill:red;
+      border: solid 1px sandybrown;
+    }
+  }
+
+</style>
+<script>
+    export default {
+        name: "svgs"
+    }
+</script>
